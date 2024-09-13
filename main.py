1dcfe2aa2f24a22396ee666fceead517936b0850
leyouyoudu93 <younesouaz@icloud.com>
2024-09-13 10:36:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+'''plus petit que exercice 7
+def lpp(a,b):
+    if a<b:
+        return(a)
+    else: 
+        return(b)
+print(lpp(1,2))'''
+
+
+
+
+''' exercice 9 savoir si oui ou non il est entier
+def  est_entier (x):
+    if x==(int):
+        return True
+    else:
+        return False
+print (est_entier(2.5))'''
+
+
+
+def est_pair(n):
+    if n%2==0:
+        return True
+    else:
+        return False
+print(est_pair(2))
