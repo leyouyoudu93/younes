370e955e50269c9581b2245fc1a241f29a8813e7
leyouyoudu93 <younesouaz@icloud.com>
2024-10-04 09:24:52 +0200

    Update repetercode.py

--- repetercode.py
+++ repetercode.py
@@ -50,3 +50,18 @@ print(sous_chaine("you","younes"))
 def triple_six(ch):
     return"666" in ch
 print(triple_six("ruivh666inàv$çu"))
+
+
+
+
+def triple_six_exact(ch):
+    n=len(ch)
+    if ch[0:3]=="666" and (n<=3 or ch[3]!="6"):
+        return True 
+    if ch[n-3:n]=="666" and ch[n-3] !='6':
+        return True
+    for k in range(1, n-3):
+     if ch[k:k+3]=="666" and ch[k-1] !=6 and ch[k+3] !=6:
+        return True 
+    return False
+print(triple_six_exact('66666'))
